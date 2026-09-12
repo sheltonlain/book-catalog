1ae457ae7c64dd28a5edb4d8e19c9916bcbe5530
Shelton Bradfute <slbradfute1@bufss.wtamu.edu>
2026-09-12 14:07:54 -0500

    Registered models in admin and populated data

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Publisher, Book, Review
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Review)
