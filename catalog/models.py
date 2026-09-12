138f5b55080cd69770efb18af652e8c71411ec4c
Shelton Bradfute <slbradfute1@bufss.wtamu.edu>
2026-09-12 13:42:57 -0500

    Added Publisher, Book, and Review models with appropriate fields

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    body = models.TextField()
+    
+    def __str__(self):
+        return f"Review for {self.book.title}"
